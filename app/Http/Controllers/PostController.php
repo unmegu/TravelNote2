a7b746c0658050fd90aabb0b6f43993d10bc77d0
unmegu <unnomgm@gmail.com>
2023-09-08 13:27:25 +0900

    create home

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return inertia("Post/Home",["posts" => $post->get()]);
+    }
+}
---
+++ resources/js/Pages/Post/Home.vue
@@ -0,0 +1,29 @@
+<script setup>
+ import Authenticated from '@/Layouts/AuthenticatedLayout.vue';
+
+ const {posts} = defineProps({
+     posts: Array
+ });
+
+</script>
+<template>
+   <Authenticated>
+       <template #header>
+           <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+               Home
+           </h2>
+       </template>
+
+       <div class="w-3/4 mx-auto mt-8">
+           <h1 class="font-bold text-3xl text-gray-800">Blog Name</h1>
+           <div class="w-1/2 mx-auto divide-gray-200 divide-y-2">  //ここから追記・変更
+               <div v-for="post in posts"
+                    class="space-y-5">
+                   <h2 class="mt-4 font-bold text-xl text-gray-800">{{post.title}}</h2>
+                   <p class="p-2">{{post.body}}</p>
+               </div>
+           </div>
+       </div>
+   </Authenticated>
+
+</template>
