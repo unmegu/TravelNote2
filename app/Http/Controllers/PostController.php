42ed8703748ea72d146c380e77d0e2f1c7456bfa
unmegu <unnomgm@gmail.com>
2023-09-28 10:43:51 +0900

    fix/緯度経度の肩をdoubleにしてmigrateし直し

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,6 +38,10 @@ class PostController extends Controller
     //use Redirect;
     public function store(PostRequest $request, Post $post)
     {
+         //cloudinaryへ画像を送信し、画像のURLを$image_urlに代入している
+        $image_url = Cloudinary::upload($request->file('image')->getRealPath())->getSecurePath();
+        dd($image_url);  //画像のURLを画面に表示
+        
         $input = $request->all();
         $input += ['user_id' => $request->user()->id];    
         $post->fill($input)->save();
--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -64,19 +64,40 @@
 
                <div v-for="post in posts"
                     class="space-y-5 p-3">
+                   
+                   
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <!--TODO:imageを追加-->
+            <!--<a href="#">-->
+            <!--    <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />-->
+            <!--</a>-->
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+
                    <div class="flex justify-between">
+                       
                        <h2 class="mt-4 font-bold text-xl text-grey-800">
                            <Link :href="route('post.show', { id: post.id })" >{{ post.title }}</Link>
                        </h2>
+                       
                        <button
                            class="px-3 bg-purple-300 hover:bg-purple-400 rounded-md"
                            @click="destroy(post)"
                        >削除
                        </button>
 
-
                    </div>
-
                    <p>{{ post.body }}</p>
                </div>
 
