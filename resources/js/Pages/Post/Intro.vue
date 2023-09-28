16c9725536dcde66fb8281c7745798f317373642
unmegu <unnomgm@gmail.com>
2023-09-28 19:40:06 +0900

    intro vue fix

--- resources/js/Pages/Post/Intro.vue
+++ resources/js/Pages/Post/Intro.vue
@@ -6,16 +6,16 @@
 
 <template>
     <!--TODO：中央ぞろえにする-->
-       <div class="flex justify-center items-center h-screen w-3/4 mx-auto mt-8">
-           <h1 class="font-bold text-3xl text-grey-800">Travel Note App へようこそ</h1>
+       <div class="h-screen w-3/4 mx-auto mt-8">
+           <h1 class="flex justify-center items-center font-bold text-3xl text-grey-800">Travel Note App へようこそ</h1>
 
            <div class="w-1/2 mx-auto divide-gray-200 divide-y-2 space-y-4">
-                <h2 class="mt-4 font-bold text-xl text-grey-800"> アプリの説明 </h2>
+                <h2 class="flex justify-center items-center mt-4 font-bold text-xl text-grey-800"> アプリの説明 </h2>
                 <!--TODO:説明をDBからとってくるようにしたほうが良い？-->
-                <p>トラベルノートアプリです！旅の記録をします！</p>
+                <p class = "flex justify-center items-center">トラベルノートアプリです！旅の記録をします！</p>
            </div>
            
-           <div>
+           <div class="flex justify-center items-center">
                <Link href="/login" method="get" as="button">使ってみる</Link>
            </div>
        </div>
